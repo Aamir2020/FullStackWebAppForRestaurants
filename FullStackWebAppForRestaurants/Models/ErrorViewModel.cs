91e22fd9422d9936fe62bbff8fb4ba4a4c428434
Aamir Zekaria Ali <aamir.zek@gmail.com>
2025-03-01 04:11:19 -0700

    Add project files.

---
+++ FullStackWebAppForRestaurants/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FullStackWebAppForRestaurants.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
